9745231aa3b83ddc15b258326181b5821706206d
jacobturner2001 <155868822+jacobturner2001@users.noreply.github.com>
2025-05-05 00:24:30 -0400

    modified script from Sandip Sky on youtube

--- archlinux.sh
+++ archlinux.sh
@@ -1,142 +1,109 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+echo "Please enter EFI paritition: (example /dev/sda1 or /dev/nvme0n1p1)"
+read EFI
 
-echo "Welcome to the Arch Linux Interactive Installer!"
+echo "Please enter Root(/) paritition: (example /dev/sda3)"
+read ROOT  
 
-# Connect to Wi-Fi using iwctl (interactive)
-echo "Launching iwctl. Please connect to your Wi-Fi manually."
-echo "Example: station wlan0 connect <SSID>"
-iwctl
+echo "Please enter your Username"
+read USER 
 
-# Verify internet connection
-echo "Verifying internet connection..."
-if ping -c 1 archlinux.org &>/dev/null; then
-    echo "Internet connection is active."
+echo "Please enter your Full Name"
+read NAME 
+
+echo "Please enter your Password"
+read PASSWORD 
+
+echo "Choose Bootloader"
+echo "1. GRUB(Default)"
+echo "2. Systemdboot"
+read -p "Enter your choice: " BOOT
+
+if [[ "$BOOT" != 2 ]]; then
+    BOOT=1
+fi
+
+# make filesystems
+echo -e "\nCreating Filesystems...\n"
+
+existing_fs=$(blkid -s TYPE -o value "$EFI")
+if [[ "$existing_fs" != "vfat" ]]; then
+    mkfs.vfat -F32 "$EFI"
+fi
+
+mkfs.ext4 "${ROOT}"
+
+# mount target
+mount "${ROOT}" /mnt
+ROOT_UUID=$(blkid -s UUID -o value "$ROOT")
+if [[ $BOOT == 2 ]]; then
+    mount --mkdir "$EFI" /mnt/boot
 else
-    echo "No internet connection detected. Please check your network and try again."
-    exit 1
+    mount --mkdir "$EFI" /mnt/boot/efi
 fi
 
-# Set keyboard layout
-read -p "Enter your preferred keyboard layout (default: us): " kblayout
-kblayout=${kblayout:-us}
-loadkeys "$kblayout"
-echo "Keyboard layout set to $kblayout."
+echo "--------------------------------------"
+echo "-- INSTALLING Base Arch Linux --"
+echo "--------------------------------------"
+pacstrap /mnt base base-devel linux linux-firmware linux-headers networkmanager wireless_tools nano intel-ucode bluez bluez-utils git --noconfirm --needed
 
-# Partition the disk
-lsblk
-read -p "Enter the disk to partition (e.g., /dev/sda): " disk
-echo "Launching cfdisk for disk partitioning..."
-cfdisk "$disk"
-
-# Format partitions
-read -p "Enter the root partition (e.g., /dev/sda1): " root_partition
-mkfs.ext4 "$root_partition"
-read -p "Enter the EFI partition (if applicable, e.g., /dev/sda2, or leave blank): " efi_partition
-if [ -n "$efi_partition" ]; then
-    mkfs.fat -F32 "$efi_partition"
-fi
-
-# Mount partitions
-mount "$root_partition" /mnt
-if [ -n "$efi_partition" ]; then
-    mkdir -p /mnt/boot/efi
-    mount "$efi_partition" /mnt/boot/efi
-fi
-
-# Install base system
-echo "Installing base system..."
-pacstrap /mnt base linux linux-firmware networkmanager sudo
-
-# Generate fstab
-echo "Generating fstab..."
-mkdir -p /mnt/etc
+# fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Collect config before chroot
-read -p "Enter hostname: " hostname
-echo "$hostname" > /mnt/hostname.txt
+cat <<REALEND > /mnt/next.sh
+useradd -m $USER
+usermod -c "${NAME}" $USER
+usermod -aG wheel,storage,power,audio,video $USER
+echo $USER:$PASSWORD | chpasswd
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+sed -i 's/^%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
 
-echo "Select your preferred desktop environment:"
-echo "1) KDE Plasma"
-echo "2) GNOME"
-echo "3) XFCE"
-echo "4) None (minimal installation)"
-read -p "Enter your choice (1-4): " de_choice
-echo "$de_choice" > /mnt/de_choice.txt
+echo "-------------------------------------------------"
+echo "Setup Language to US and set locale"
+echo "-------------------------------------------------"
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-# Chroot into the system and finish setup
-arch-chroot /mnt /bin/bash <<'EOF'
-set -e
-
-# Set timezone
-ln -sf /usr/share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime
+ln -sf /usr/share/zoneinfo/America/indiana/indianapolis /etc/localtime
 hwclock --systohc
 
-# Set locale
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# Set hostname
-hostname=$(cat /hostname.txt)
-echo "$hostname" > /etc/hostname
-{
-echo "127.0.0.1   localhost"
-echo "::1         localhost"
-echo "127.0.1.1   $hostname.localdomain $hostname"
-} >> /etc/hosts
-
-# Set root password
-echo "Set root password:"
-passwd
-
-# Enable network manager
-systemctl enable NetworkManager
-
-# Install bootloader
-if mount | grep -q "/boot/efi"; then
-    pacman -S --noconfirm grub efibootmgr
-    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-else
-    pacman -S --noconfirm grub
-    grub-install --target=i386-pc /dev/sda
-fi
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Install desktop environment
-de_choice=$(cat /de_choice.txt)
-case "$de_choice" in
-    1)
-        echo "Installing KDE Plasma..."
-        pacman -S --noconfirm xorg plasma kde-applications sddm
-        systemctl enable sddm.service
-        ;;
-    2)
-        echo "Installing GNOME..."
-        pacman -S --noconfirm xorg gnome gnome-extra gdm
-        systemctl enable gdm.service
-        ;;
-    3)
-        echo "Installing XFCE..."
-        pacman -S --noconfirm xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
-        systemctl enable lightdm.service
-        ;;
-    4)
-        echo "Skipping desktop environment installation."
-        ;;
-    *)
-        echo "Invalid choice. Skipping desktop environment installation."
-        ;;
-esac
+echo "archlinux" > /etc/hostname
+cat <<EOF > /etc/hosts
+127.0.0.1	localhost
+::1			localhost
+127.0.1.1	archlinux.localdomain	archlinux
 EOF
 
-# Clean up temp files
-rm -f /mnt/hostname.txt /mnt/de_choice.txt
+echo "-------------------------------------------------"
+echo "Audio Drivers"
+echo "-------------------------------------------------"
 
-# Unmount and reboot
-umount -R /mnt
-echo "Installation complete! Rebooting..."
-reboot
-# End of script
+pacman -S mesa-utils nvidia nvidia-utils nvidia-settings opencl-nvidia nvidia-prime pipewire pipewire-alsa pipewire-pulse --noconfirm --needed
+
+systemctl enable NetworkManager bluetooth
+systemctl --user enable pipewire pipewire-pulse
+
+echo "--------------------------------------"
+echo "-- Bootloader Installation  --"
+echo "--------------------------------------"
+
+if [[ $BOOT == 2 ]]; then
+    bootctl install --path=/boot
+    echo "default arch.conf" >> /boot/loader/loader.conf
+    cat <<EOF > /boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=UUID=$ROOT_UUID rw quiet
+EOF
+else
+    pacman -S grub efibootmgr --noconfirm --needed
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Linux Boot Manager"
+    grub-mkconfig -o /boot/grub/grub.cfg
+fi
+echo "-------------------------------------------------"
+echo "Install Complete, You can reboot now"
+echo "-------------------------------------------------"
+REALEND
